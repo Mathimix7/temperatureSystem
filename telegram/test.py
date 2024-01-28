8edd232bf780f8ad55912e41c4cf260b4a204713
Mathimix <mathimixich@gmail.com>
2024-01-27 19:01:34 -0500

    Delete unnecessary files and code

--- telegram/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-groupNumber = 1
-RequestLink = f'http://192.168.68.103/api/tsbtK0JdOtfyfIZh64L8RJPMQM70Ght5GRl7stYT/groups/{groupNumber}'
-groupBrightness = requests.get(RequestLink).json()
-groupBrightness = groupBrightness['action']['bri']
-groupBrightnessDecrease = 31
-print(groupBrightness, str(int(groupBrightness - groupBrightnessDecrease)))
-a = requests.put(f'http://192.168.68.103/api/tsbtK0JdOtfyfIZh64L8RJPMQM70Ght5GRl7stYT/groups/{groupNumber}/action', data='{"bri":' + str(int(groupBrightness - groupBrightnessDecrease)) + '}')
-print(a.json())
--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-request = requests.get(f"http://192.168.68.136:5555/device/{1}").json()
-print(request)

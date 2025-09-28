7be134049da7680b09b6140c38548832df0e9303
Mathimix <mathimixich@gmail.com>
2025-09-28 13:28:03 -0400

    Fix issues with threads

--- micropython/secondVersion.py
+++ micropython/secondVersion.py
@@ -55,9 +55,60 @@ try:
 
     tempOutside = "N/A"
     humOutside = "N/A"
+    temperatureNow = "N/A"
+    temperatureNowOutside = "N/A"
+    batteryValue = None
     corf = "C"
     threadStartedWIFI = False
     threadStartedAPI = False
+    state_lock = _thread.allocate_lock()
+    TEMP_TOGGLE_INTERVAL_MS = 5000
+    last_temp_toggle_ms = time.ticks_ms()
+
+    def set_api_status(value):
+        global apiON
+        state_lock.acquire()
+        try:
+            apiON = value
+        finally:
+            state_lock.release()
+
+    def get_api_status():
+        state_lock.acquire()
+        try:
+            return apiON
+        finally:
+            state_lock.release()
+
+    def set_wifi_thread_flag(value):
+        global threadStartedWIFI
+        state_lock.acquire()
+        try:
+            threadStartedWIFI = value
+        finally:
+            state_lock.release()
+
+    def is_wifi_thread_running():
+        state_lock.acquire()
+        try:
+            return threadStartedWIFI
+        finally:
+            state_lock.release()
+
+    def set_api_thread_flag(value):
+        global threadStartedAPI
+        state_lock.acquire()
+        try:
+            threadStartedAPI = value
+        finally:
+            state_lock.release()
+
+    def is_api_thread_running():
+        state_lock.acquire()
+        try:
+            return threadStartedAPI
+        finally:
+            state_lock.release()
 
     def resetAVGS():
         global tempAverage, humAverage, tempOutsideAVG, humOutsideAVG
@@ -76,53 +127,57 @@ try:
             hum = str(int(hum[1:])/10)
             hum = hum.split(".")
             hum = hum[0] + "." + hum[1][:1]
+            print(sid)
             return float(temp), float(hum), sid
         except:
             print(data)
 
     def checkForTempType(temp, corf, second=None):
-        if second == None:
-            if corf == "F":
-                temperatureNow = str(round(temp*9/5 + 32, 2))
-            else:
-                temperatureNow = str(round(temp, 1))
-            temperatureNow = temperatureNow.split(".")
-            temperatureNow = temperatureNow[0] + "." + temperatureNow[1][:1]
-            return temperatureNow, corf
-        if second % 5 == 0:
-            if corf == "F":
-                corf = "C"
-            else:
-                corf = "F"
+        global last_temp_toggle_ms
+        if temp is None:
+            return "N/A", corf
+
+        if second is not None:
+            now_ms = time.ticks_ms()
+            if time.ticks_diff(now_ms, last_temp_toggle_ms) >= TEMP_TOGGLE_INTERVAL_MS:
+                last_temp_toggle_ms = now_ms
+                corf = "C" if corf == "F" else "F"
+
         if corf == "F":
-            temperatureNow = str(round(temp*9/5 + 32, 2))
+            temperature_now = str(round(temp * 9 / 5 + 32, 2))
         else:
-            temperatureNow = str(round(temp, 1))
-        temperatureNow = temperatureNow.split(".")
-        temperatureNow = temperatureNow[0] + "." + temperatureNow[1][:1]
-        return temperatureNow, corf
+            temperature_now = str(round(temp, 1))
+
+        temperature_now = temperature_now.split(".")
+        if len(temperature_now) == 1:
+            temperature_now.append("0")
+        temperature_now = temperature_now[0] + "." + temperature_now[1][:1]
+        return temperature_now, corf
 
     def connectThread():
-        global apiON
-        wm.connect()
-        threadStartedWIFI = False
         try:
-            timeNow = urequests.get(f"http://{ip}:{port}/time").json()
+            wm.connect()
+            response = urequests.get(f"http://{ip}:{port}/time")
+            timeNow = response.json()
+            response.close()
             rtc.datetime((int(timeNow["year"]), int(timeNow["month"]), int(timeNow["day"]), int(timeNow["weekday"]), int(timeNow["hour"]), int(timeNow["minute"]), int(timeNow["second"]), 0))
-            apiON = True
+            set_api_status(True)
             resetAVGS()
-        except:
-            apiON = False 
+        except Exception as e:
+            print("Wi-Fi reconnect failed:", e)
+            set_api_status(False)
+        finally:
+            set_wifi_thread_flag(False)
 
     def checkAPIstatus():
         while True:
             print("a")
-            global apiON
             try:
-                timeNow = urequests.get(f"http://{ip}:{port}/time").json()
+                response = urequests.get(f"http://{ip}:{port}/time")
+                timeNow = response.json()
+                response.close()
                 rtc.datetime((int(timeNow["year"]), int(timeNow["month"]), int(timeNow["day"]), int(timeNow["weekday"]), int(timeNow["hour"]), int(timeNow["minute"]), int(timeNow["second"]), 0))
-                apiON = True
-                threadStartedAPI = False
+                set_api_status(True)
                 resetAVGS()
                 break
             except Exception as e:
@@ -132,8 +187,9 @@ try:
                 icons.wifiSymbol(display, 114,0)
                 display.printstring(0,20,f"API failed.\nTrying to connect...\n{e}", REFSAN12)
                 display.show()
-                apiON = False
+                set_api_status(False)
             time.sleep(5)
+        set_api_thread_flag(False)
     lastSecond = 0
     while True:
         if stop_button.value() == 0:
@@ -141,16 +197,16 @@ try:
             display.printstring(0, 0, f"Stoping...\nButton pressed!", REFSAN12)
             display.show()
             sys.exit()
-        if wm.is_connected() and apiON:
+        if wm.is_connected() and get_api_status():
             timeNow = rtc.datetime()
-            month,day,hour, minute, second = timeNow[1], timeNow[2], timeNow[4], timeNow[5], timeNow[6]
-            print(second)
-            if lastSecond != second-1:
+            month, day, hour, minute, second = timeNow[1], timeNow[2], timeNow[4], timeNow[5], timeNow[6]
+            if lastSecond != second - 1:
                 currentSecond = second
                 second = lastSecond + 1
-                lastSecond = currentSecond 
+                lastSecond = currentSecond
             else:
                 lastSecond = second
+
             if second % 5 == 0:
                 try:
                     headers = {
@@ -159,65 +215,79 @@ try:
                         "humidity": round(sensor.relative_humidity)
                     }
                     response = urequests.post(f"http://{ip}:{port}/updateCurrent/0", json=headers)
-                    if temperatureNowOutside != "N/A":
+                    response.close()
+                    if isinstance(tempOutside, (int, float)):
                         headers = {
                             "macAddress": macAddress,
                             "temp": round(tempOutside, 1),
                             "humidity": round(humOutside)
                         }
                         response = urequests.post(f"http://{ip}:{port}/updateCurrent/1", json=headers)
-                except:
-                    print("could not send current temp")
+                        response.close()
+                except Exception as e:
+                    print("could not send current temp", e)
+
             display.fill(0)
             display2.fill(0)
-            display.hline(0,14,128,0xffff)
-            display2.hline(0,14,128,0xffff)
-            icons.wifiSymbol(display, 114,0)
-            icons.wifiSymbol(display2, 114,0)
-            tempAverage = (tempAverage + sensor.temperature)/2
-            humAverage = (humAverage + sensor.relative_humidity)/2
+            display.hline(0, 14, 128, 0xffff)
+            display2.hline(0, 14, 128, 0xffff)
+            icons.wifiSymbol(display, 114, 0)
+            icons.wifiSymbol(display2, 114, 0)
+            tempAverage = (tempAverage + sensor.temperature) / 2
+            humAverage = (humAverage + sensor.relative_humidity) / 2
+
             if len(str(timeNow[4])) == 1:
                 hour = f"0{str(timeNow[4])}"
             if len(str(timeNow[5])) == 1:
-                minute = str(minute)
-                minute = "0"+minute
+                minute = "0" + str(minute)
             if len(str(timeNow[1])) == 1:
                 month = f"0{str(timeNow[1])}"
             if len(str(timeNow[2])) == 1:
                 day = f"0{str(timeNow[2])}"
+
             display.text(f"{day}/{month}", 43, 0)
             display2.text(f"{hour}:{minute}", 43, 0)
-            fill = round(sensor.temperature//2.5)
+
+            fill = round(sensor.temperature // 2.5)
             if uart.any():
                 try:
-                    tempOutside, humOutside, batteryValue = formatUARTdata(uart.read())
-                except:
-                    pass
-                if tempOutsideAVG == None:
-                    tempOutsideAVG = tempOutside
-                if humOutsideAVG == None:
-                    humOutsideAVG = humOutside
-                tempOutsideAVG = (tempOutside + tempOutsideAVG)/2
-                humOutsideAVG = (humOutside + humOutsideAVG)/2
-            display.text("IN",0,0)
+                    parsed = formatUARTdata(uart.read())
+                    if parsed:
+                        tempOutside, humOutside, batteryValue = parsed
+                        if tempOutsideAVG is None:
+                            tempOutsideAVG = tempOutside
+                        else:
+                            tempOutsideAVG = (tempOutside + tempOutsideAVG) / 2
+                        if humOutsideAVG is None:
+                            humOutsideAVG = humOutside
+                        else:
+                            humOutsideAVG = (humOutside + humOutsideAVG) / 2
+                except Exception as e:
+                    print("ERROR reading uart:", e)
+            else:
+                print("NO UART DATA")
+
+            display.text("IN", 0, 0)
             icons.temperatureSymbol(display, 30, 20, fill)
             data = checkForTempType(sensor.temperature, corf, second)
             if data:
                 temperatureNow, corf = data
             display.printstring(45, 20, f"{temperatureNow} {corf}", ariblk)
-            icons.humiditySymbol(display, 27,40)
+            icons.humiditySymbol(display, 27, 40)
             display.printstring(45, 42, f"{round(sensor.relative_humidity, 1)}%", ariblk)
-            if not isinstance(tempOutside, str):
+
+            if isinstance(tempOutside, (int, float)):
                 temperatureNowOutside, _ = checkForTempType(tempOutside, corf)
-                fill = round(tempOutside//2.5)
+                fill = round(tempOutside // 2.5)
             else:
                 temperatureNowOutside = tempOutside
                 fill = 0
+
             icons.temperatureSymbol(display2, 30, 20, fill)
-            icons.humiditySymbol(display2, 27,40)
-            display2.text("OUT",0,0)
+            icons.humiditySymbol(display2, 27, 40)
+            display2.text("OUT", 0, 0)
             display2.printstring(45, 20, f"{temperatureNowOutside} {corf}", ariblk)
-            print(humOutside)
+
             if humOutside != "N/A":
                 hum = str(humOutside)
                 hum = hum.split(".")
@@ -225,18 +295,22 @@ try:
             else:
                 hum = humOutside
             display2.printstring(45, 42, f"{hum} %", ariblk)
+
             display.show()
             display2.show()
+
             with open("sent.dat") as f:
                 status = f.read()
-            if str(timeNow[4]*60 + timeNow[5]).endswith("0"):
+
+            if str(timeNow[4] * 60 + timeNow[5]).endswith("0"):
                 if status == "false":
-                    sent = True
                     with open("sent.dat", "w") as f:
                         f.write("true")
                     print("Time to send")
                     try:
-                        timeNow = urequests.get(f"http://{ip}:{port}/time").json()
+                        response = urequests.get(f"http://{ip}:{port}/time")
+                        timeNow = response.json()
+                        response.close()
                         rtc.datetime((int(timeNow["year"]), int(timeNow["month"]), int(timeNow["day"]), int(timeNow["weekday"]), int(timeNow["hour"]), int(timeNow["minute"]), int(timeNow["second"]), 0))
                         headers = {
                             "macAddress": macAddress,
@@ -246,9 +320,10 @@ try:
                             "humidity": round(humAverage)
                         }
                         response = urequests.post(f"http://{ip}:{port}/create/0", json=headers)
+                        response.close()
                         tempAverage = sensor.temperature
                         humAverage = sensor.relative_humidity
-                        if tempOutsideAVG != None:
+                        if tempOutsideAVG is not None:
                             headers = {
                                 "macAddress": macAddress,
                                 "time": int(timeNow["unix"]),
@@ -257,6 +332,7 @@ try:
                                 "humidity": round(humOutsideAVG)
                             }
                             response = urequests.post(f"http://{ip}:{port}/create/1", json=headers)
+                            response.close()
                             tempOutsideAVG = None
                             humOutsideAVG = None
                         else:
@@ -264,45 +340,55 @@ try:
                             humOutside = "N/A"
                     except Exception as e:
                         print(e)
-                        apiON = False
+                        set_api_status(False)
+
                     try:
-                        response = urequests.post(f"http://{ip}:{port}/updateBattery/1/{batteryValue}")
-                    except:
-                        print("Did not send value")
-                        
+                        if batteryValue:
+                            battery_numeric = str(int(batteryValue[1:]))
+                            response = urequests.post(f"http://{ip}:{port}/updateBattery/1/{battery_numeric}")
+                            response.close()
+                    except Exception as e:
+                        print("Did not send value", e)
             else:
                 if status == "true":
                     with open("sent.dat", "w") as f:
                         f.write("false")
         else:
             display2.fill(0)
-            display2.hline(0,14,128,0xffff)
+            display2.hline(0, 14, 128, 0xffff)
             timeNow = rtc.datetime()
             second = timeNow[6]
             display2.text("IN", 25, 2)
             display2.text("OUT", 85, 2)
+
             if uart.any():
                 try:
-                    tempOutside, humOutside, _ = formatUARTdata(uart.read())
+                    parsed = formatUARTdata(uart.read())
+                    if parsed:
+                        tempOutside, humOutside, _ = parsed
                 except Exception as e:
-                    print(e)
-            fill = round(sensor.temperature//2.5)
+                    print("ERROR reading uart:", e)
+
+            fill = round(sensor.temperature // 2.5)
             icons.temperatureSymbol(display2, 3, 20, fill)
             data = checkForTempType(sensor.temperature, corf, second)
             if data:
                 temperatureNow, corf = data
             display2.printstring(18, 20, f"{temperatureNow} {corf}", REFSAN12)
-            icons.humiditySymbol(display2, 0,40)
+            icons.humiditySymbol(display2, 0, 40)
             display2.printstring(18, 42, f"{round(sensor.relative_humidity, 1)}%", REFSAN12)
-            if not isinstance(tempOutside, str):
+
+            if isinstance(tempOutside, (int, float)):
                 temperatureNowOutside, _ = checkForTempType(tempOutside, corf)
-                fill = round(tempOutside//2.5)
+                fill = round(tempOutside // 2.5)
             else:
                 temperatureNowOutside = tempOutside
                 fill = 0
+
             icons.temperatureSymbol(display2, 68, 20, fill)
-            icons.humiditySymbol(display2, 65,40)
+            icons.humiditySymbol(display2, 65, 40)
             display2.printstring(83, 20, f"{temperatureNowOutside} {corf}", REFSAN12)
+
             if humOutside != "N/A":
                 hum = str(humOutside)
                 hum = hum.split(".")
@@ -310,26 +396,33 @@ try:
             else:
                 hum = humOutside
             display2.printstring(83, 42, f"{hum} %", REFSAN12)
+
             display2.show()
+
             if not wm.is_connected():
-                if not threadStartedWIFI:
-                    _thread.start_new_thread(connectThread, ())
-                    threadStartedWIFI = True
-            elif not apiON:
+                if not is_wifi_thread_running():
+                    set_wifi_thread_flag(True)
+                    try:
+                        _thread.start_new_thread(connectThread, ())
+                    except Exception as e:
+                        print("Failed to start Wi-Fi thread:", e)
+                        set_wifi_thread_flag(False)
+            elif not get_api_status():
                 display.fill(0)
-                display.hline(0,14,128,0xffff)
-                icons.wifiSymbol(display, 114,0)
-                display.printstring(0,20,"API failed.\nTrying to connect...", REFSAN12)
+                display.hline(0, 14, 128, 0xffff)
+                icons.wifiSymbol(display, 114, 0)
+                display.printstring(0, 20, "API failed.\nTrying to connect...", REFSAN12)
                 display.show()
-                if not threadStartedAPI:
-                    _thread.start_new_thread(checkAPIstatus, ())
-                    threadStartedAPI = True
-                
+                if not is_api_thread_running():
+                    set_api_thread_flag(True)
+                    try:
+                        _thread.start_new_thread(checkAPIstatus, ())
+                    except Exception as e:
+                        print("Failed to start API thread:", e)
+                        set_api_thread_flag(False)
+
         time.sleep(1)
 
 except Exception as e:
     print(e)
-    #machine.reset()
-
-
-
+    machine.reset()
